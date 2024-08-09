c81153036e023f58a5c96c038c5546e5c3ced5de
Mohamed Soumare <mohamedsoumare17763@gmail.com>
2024-08-09 12:13:24 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <ListEmployee />
+  </div>
+</template>
+
+<script>
+import ListEmployee from './components/ListEmployee.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ListEmployee,
+  },
+};
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+
+h1 {
+  font-size: 24px;
+  color: #121212;
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/components/ListEmployee.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1>Liste des employés</h1>
+    <ol>
+      <li 
+        v-for="employe in employes" :key="employe.id" 
+        @mouseover="hover = employe.id"
+        @mouseleave="hover = null"
+        :class="{ 'hovered': hover === employe.id }"
+      >
+      {{ employe.nom }}
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmployeList',
+  data() {
+    return {
+      employes: [
+        { id: 1, nom: 'Jean Dupont' },
+        { id: 2, nom: 'Marie Durand' },
+        { id: 3, nom: 'Pierre Martin' },
+      ],
+      hover: null,
+    };
+  },
+};
+</script>
+
+<style scoped>
+ol {
+  padding-left: 20px;
+}
+
+li {
+  padding: 10px;
+  margin: 5px 0;
+  list-style-type: decimal;
+  transition: background-color 0.3s ease;
+}
+
+li.hovered {
+  background-color: #25fd04;
+}
+</style>
